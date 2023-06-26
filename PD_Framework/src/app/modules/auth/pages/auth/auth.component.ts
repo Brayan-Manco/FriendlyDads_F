5eaa6011a1107d83c78b426599878c019e5c3481
Brayan Andres <manco300mz@gmail.com>
2023-06-25 22:32:14 -0500

    aa

--- PD_Framework/src/app/modules/auth/pages/auth/auth.component.ts
+++ PD_Framework/src/app/modules/auth/pages/auth/auth.component.ts
@@ -3,6 +3,7 @@ import Swal from 'sweetalert2';
 import { cuentaService } from '../../services/services.service';
 import { Router } from '@angular/router';
 import { Login } from 'src/app/interfaces/tbl_cuenta';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
@@ -13,6 +14,7 @@ export class AuthComponent implements OnInit {
   
   correo: string ='';
   contrasena: string= '';
+  loading: boolean= false;
 
   constructor(private _cuentaService: cuentaService,
     private router: Router ){}
@@ -33,12 +35,29 @@ export class AuthComponent implements OnInit {
       contrasena: this.contrasena,
     }
 
+    this.loading =true;
     this._cuentaService.login(login).subscribe({
       next: (token) => {
         localStorage.setItem('token', token);
         this.router.navigate(['/menu'])
         console.log(token)
+        },
+        error:(e: HttpErrorResponse)=>{
+          this.msjError(e);
+          this.loading = false;
         }
       })
+    }
+
+    msjError(e: HttpErrorResponse){
+      if(e.error.msg){
+        Swal.fire({icon: 'error',
+        title: 'Oops...',
+        text: `${e.error.msg}`})
+      }else{
+        Swal.fire({icon: 'error',
+        title: 'Oops...',
+        text: 'Ha ocurrido un error, por favor comuniquese con el adminstrador'})
+      }
     }
 }
