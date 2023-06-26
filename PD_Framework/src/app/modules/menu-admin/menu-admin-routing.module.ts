24b1e5240da4d02a99e438a9a4c2906fdfed0aab
Brayan Andres <jhoncarofernandez@gmail.com>
2023-06-26 18:03:17 -0500

    b

--- PD_Framework/src/app/modules/menu-admin/menu-admin-routing.module.ts
+++ PD_Framework/src/app/modules/menu-admin/menu-admin-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuAdminComponent } from './pages/menu-admin/menu-admin.component'
-import { FormFComponent } from 'src/app/shared/components/form-f/form-f.component';
+
 
 const routes: Routes = [
   {
--- PD_Framework/src/app/shared/shared.module.ts
+++ PD_Framework/src/app/shared/shared.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FormBComponent } from './components/form-b/form-b.component';
-import { FormPComponent } from './components/form-p/form-p.component';
-import { FormFComponent } from './components/form-f/form-f.component';
+
+
+
 import { RouterModule } from '@angular/router';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 
@@ -17,9 +17,7 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
   declarations: [
     FooterComponent,
     HeaderComponent,
-    FormBComponent,
-    FormPComponent,
-    FormFComponent,
+    
     SpinnerComponent,
   ],
   imports: [
@@ -29,9 +27,7 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
   exports: [
     FooterComponent,
     HeaderComponent,
-    FormBComponent,
-    FormPComponent,
-    FormFComponent,
+
     SpinnerComponent
   ]
 })
